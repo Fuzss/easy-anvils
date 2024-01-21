80457a06c46bd698ab31b4ab77a6cfa7f0e99969
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-21 19:20:00 +0100

    prepare 1.20.4 port

--- 1.20/Common/src/main/java/fuzs/easyanvils/config/ClientConfig.java
+++
@@ -1,9 +0,0 @@
-package fuzs.easyanvils.config;
-
-import fuzs.puzzleslib.api.config.v3.Config;
-import fuzs.puzzleslib.api.config.v3.ConfigCore;
-
-public class ClientConfig implements ConfigCore {
-    @Config(description = "Render inventory contents of an anvil.")
-    public boolean renderAnvilContents = true;
-}
